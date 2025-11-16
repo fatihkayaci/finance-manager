fd6b9958d5ea5123bf8a40c79dfddc7dffba5106
Fatih Kayacı <fatihkayaci@yahoo.com>
2025-11-16 09:45:58 +0300

    feat: refactor-professional.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,8 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-import SideBar from './components/SideBar';
-
-import Dashboard from './pages/Dashboard';
-import Reports from './pages/Reports';
-import Transaction from './pages/Transactions';
-import CategoryPage from './pages/CategoryPage';
-
 function App() {
   return (
-    <BrowserRouter>
-      <div className='container'>
-        <div className="container-left">
-          <SideBar />
-        </div>
-
-        <div className="container-right">
-          <Routes>
-            <Route path="/" element={<Navigate to="/Dashboard" replace />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/income" element={<Transaction type="income"/>} />
-            <Route path="/expense" element={<Transaction type="expense"/>} />
-            <Route path="/reports" element={<Reports />} />
-            <Route path="/categoryPage" element={<CategoryPage />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+    <>
+    new project
+    </>
     
   )
 }
